dda9946b31f6fb63de3ac40d09a88e32b4f3a4ea
TranDangHoaiNam <namwe300@gmail.com>
2024-03-26 07:06:30 +0700

    finish 1b

--- App.js
+++ App.js
@@ -1,13 +1,16 @@
 import React, { useState, useEffect } from 'react';
-import { Animated, Text, View } from 'react-native';
+import { Animated, Text, View, Image } from 'react-native';
 
 const WelcomeText = () => {
   const [opacity] = useState(new Animated.Value(0)); // Giá trị mặc định cho opacity là 0
-  const welcomeMessage = 'You are welcome.';
+  const [ballY] = useState(new Animated.Value(-100)); // Giá trị mặc định cho vị trí ban đầu của quả bóng
+
+  const welcomeMessage = 'You are welcome!.';
   const delay = 3000 / welcomeMessage.length;
 
   useEffect(() => {
     animateWelcomeText(); // Gọi hàm animateWelcomeText khi component được mount
+    animateBall(); // Gọi hàm animateBall khi component được mount
   }, []);
 
   const animateWelcomeText = () => {
@@ -21,16 +24,36 @@ const WelcomeText = () => {
     ).start();
   };
 
+  const animateBall = () => {
+    Animated.timing(
+      ballY,
+      {
+        toValue: 200, // Di chuyển từ vị trí -100 (trên cùng) xuống vị trí 200 (dưới cùng)
+        duration: 3000, // Thời gian để hoàn thành animation là 3 giây
+        useNativeDriver: true // Sử dụng driver native để tối ưu hiệu suất
+      }
+    ).start();
+  };
+
   return (
-    <View>
+    <View style={{ alignItems: 'center' }}>
       <Animated.Text
         style={{
           opacity: opacity,
-          fontSize: 24 // Đặt kích thước phù hợp
+          fontSize: 24, // Đặt kích thước phù hợp
+          marginBottom: 20 // Khoảng cách dưới của văn bản
         }}
       >
         {welcomeMessage}
       </Animated.Text>
+      <Animated.Image
+        source={require('./assets/bongda.png')}
+        style={{
+          width: 50, // Đặt chiều rộng của hình ảnh
+          height: 50, // Đặt chiều cao của hình ảnh
+          transform: [{ translateY: ballY }] // Áp dụng transform translateY cho di chuyển
+        }}
+      />
     </View>
   );
 };
